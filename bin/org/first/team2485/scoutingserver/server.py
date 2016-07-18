f04f8355400358a39c2fae944f634498d3fc8da0
Nicholas Contreras <ncontreras2018@francisparker.org>
2016-07-18 12:15:44 -0700

    More formatting

--- bin/org/first/team2485/scoutingserver/server.py
+++ bin/org/first/team2485/scoutingserver/server.py
@@ -33,58 +33,52 @@ while True:
   	except Exception:
     		print("no incoming connections")
 
-  consoleInput = input("I")
-  print("Input is: " + consoleInput)
-  queuedBroadcasts.append(consoleInput)
+	consoleInput = input("I")
+	print("Input is: " + consoleInput)
+	queuedBroadcasts.append(consoleInput)
 
-  broadcastToSend = "IGNORE"
+	broadcastToSend = "IGNORE"
 
-  if len(queuedBroadcasts) > 0:
-    broadcastToSend = queuedBroadcasts[0]
-    queuedBroadcasts.remove(broadcastToSend)
+	if len(queuedBroadcasts) > 0:
+	    	broadcastToSend = queuedBroadcasts[0]
+	    	queuedBroadcasts.remove(broadcastToSend)
 
-  for curSocket in connectedSockets:
-    try:
-        ready_to_read, ready_to_write, in_error = \
-            select.select([curSocket], [curSocket], [curSocket], 1)
-    except select.error:
-        conn.shutdown(2)    # 0 = done receiving, 1 = done sending, 2 = both
-        conn.close()
-        connectedSockets.remove(curSocket)
-        # connection error event here, maybe reconnect
-        print ("connection error")
-        break
-    	if len(ready_to_read) > 0:
-    	if ( not (recv.find("BROADCAST")) and ( not (recv.find("SendToServer")))):
-        	recv = conn.recv(2048)
-        	millis = int(round(time.time() * 1000))
-        	newFile = open(str(millis) + ".csv", "w")
-        	newFile.write(recv)
-        	newFile.close()
-
-	if recv.find("BROADCAST") != -1:
-	        	# do stuff with received data
-	        	queuedBroadcasts.append(recv)
-	          	print ("recieved: " + recv)
-
-	if recv.find("SendToServer") != -1:
-	        	recv = conn.recv(2048)
-	        	print ("received:" + recv)
-	        	def replyToClient():
-	        		master = Tk();
-	        		inputReply = Entry(master)
-	        		inputReply.pack()
-	        		inputReply.focus_set()
-	        		serverInput=inputReply.get()
-	        		curSocket.send(serverInput)
-	        		print ("Message Sent")
-
-        	top = Tkinter.Tk()
-        	serverReply = Tkinter.Button(top, text="ReplyToDirectMessage", command=replyToClient)
-        	serverReply.pack()
-        	top.mainloop()
-
-    if len(ready_to_write) > 0:
-        # connection established, send some stuff
-        if broadcastToSend != "IGNORE":
-          conn.send(broadcastToSend)
+	  	for curSocket in connectedSockets:
+	    		try:
+	        			ready_to_read, ready_to_write, in_error = select.select([curSocket], [curSocket], [curSocket], 1)
+	        		except select.error: # connection error event here, maybe reconnect
+	        			conn.shutdown(2)    # 0 = done receiving, 1 = done sending, 2 = both
+	        			conn.close()
+	        			connectedSockets.remove(curSocket)
+	        			print ("connection error")
+	        			continue
+	    		if len(ready_to_read) > 0:
+	    			if ( not (recv.find("BROADCAST")) and ( not (recv.find("SendToServer")))):
+	        				recv = conn.recv(2048)
+	        				millis = int(round(time.time() * 1000))
+	        				newFile = open(str(millis) + ".csv", "w")
+	        				newFile.write(recv)
+	        				newFile.close()
+	        			if recv.find("BROADCAST") != -1:
+		        			# do stuff with received data
+		        			queuedBroadcasts.append(recv)
+		          			print ("recieved: " + recv)
+		          		if recv.find("SendToServer") != -1:
+		        			recv = conn.recv(2048)
+		        			print ("received:" + recv)
+		        			def replyToClient():
+		        				master = Tk();
+		        				inputReply = Entry(master)
+		        				inputReply.pack()
+		        				inputReply.focus_set()
+		        				serverInput=inputReply.get()
+		        				curSocket.send(serverInput)
+		        				print ("Message Sent")
+					top = Tkinter.Tk()
+	        				serverReply = Tkinter.Button(top, text="ReplyToDirectMessage", command=replyToClient)
+	        				serverReply.pack()
+	        				top.mainloop()
+			if len(ready_to_write) > 0:
+	        			# connection for sending is valid, send the next item
+	        			if broadcastToSend != "IGNORE":
+	          				conn.send(broadcastToSend)
--- src/org/first/team2485/scoutingform/formIO/client.py
+++ src/org/first/team2485/scoutingform/formIO/client.py
@@ -16,68 +16,67 @@ print("Looking for scouting server....")
 service_matches = find_service( uuid = uuid, address = addr )
 
 if len(service_matches) == 0:
-    print("Could not find Server")
-    sys.exit(1)
+	print("Could not find Server")
+	sys.exit(1)
 
 port
 name
 host
 
 for x in xrange(0, len(service_matches)):
+	match = service_matches[x]
+	port = match["port"]
+	name = match["name"]
+	host = match["host"]
 
-    match = service_matches[x]
-    port = match["port"]
-    name = match["name"]
-    host = match["host"]
-
-    if name == "Scouting Server":
-        break;
+	if name == "Scouting Server":
+        		break;
 
 print("Connecting to Port: \"%s\" Name: \"%s\" Host: \"%s\" " % (port, name, host))
 
 sock=BluetoothSocket( RFCOMM )
 try:
-    sock.connect(host, port)
+	sock.connect(host, port)
 except:
-    print ("failed to connect - Port: \"%s\" Name: \"%s\" Host: \"%s\" " % (port, name, host))
-    sys.exit()
+    	print ("failed to connect - Port: \"%s\" Name: \"%s\" Host: \"%s\" " % (port, name, host))
+    	sys.exit()
 
-t=Timer(10.0. receiveFromServer)
+t=Timer(10.0 receiveFromServer)
 t.start()
 
 while True:
-    consoleInput = input("I")
-    print("Input is: " + consoleInput)
+	consoleInput = input("I")
+	print("Input is: " + consoleInput)
 
-    if consoleInput.find("BROADCAST") != -1:
-        sock.send(consoleInput)
-    elif consoleInput.find("SendToServer") !=-1:
-        sock.send(consoleInput)
-    else:
-        file = open("unsentData/scoutingData.csv", "r")
-        data = file.read()
-        data.close()
-        print("Sending: " + data)
-        sock.send(data)
-        print("Sent")
-        millis = int(round(time.time() * 1000))
-        newFile = open(str(millis) + ".csv", "w")
-        newFile.write(data)
-        newFile.close()
+	if consoleInput.find("BROADCAST") != -1:
+        		sock.send(consoleInput)
+    	elif consoleInput.find("SendToServer") !=-1:
+        		sock.send(consoleInput)
+    	else:
+        		file = open("unsentData/scoutingData.csv", "r")
+        		data = file.read()
+        		data.close()
+        		print("Sending: " + data)
+        		sock.send(data)
+        		print("Sent")
+        		millis = int(round(time.time() * 1000))
+        		newFile = open(str(millis) + ".csv", "w")
+        		newFile.write(data)
+        		newFile.close()
 
 
 def receiveFromServer():
-    try:
-        ready_to_read,ready_to_write,in_error = select.select([sock], [sock], [sock], 1)
-    except select.error:
-        print ("Error in Python 'Select'")
-        pass
-    if ready_to_read >0:
-        recv = conn.recv(2048)
-        print (recv)
-    if not (ready_to_read or ready_to_write or in_error):
-        print "no server message" 
-    t.start()
+	try:
+        		ready_to_read, ready_to_write, in_error = select.select([sock], [sock], [sock], 1)
+        	except select.error:
+        		print ("Error in Python 'Select'")
+        		return
+    	if ready_to_read >0:
+        		recv = conn.recv(2048)
+        		print (recv)
+    	if not (ready_to_read or ready_to_write or in_error):
+        		print "no server message" 
+   		t.start()
 
 
 
